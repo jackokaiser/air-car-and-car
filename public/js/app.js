b46cb46fafe0e28bb1ee6a1bbd22249e117772f0
Jacques KAISER <jacko.kaiser@gmail.com>
2014-02-08 12:01:40 +1100

    emacs complant syntax with .

--- public/js/app.js
+++ public/js/app.js
@@ -3,28 +3,28 @@
 // Declare app level module which depends on filters, and services
 
 angular.module('myApp', [
-  'myApp.controllers',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives'
-]).
-config(function ($routeProvider, $locationProvider) {
-  $routeProvider.
-    when('/cars', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/map', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
-    }).
-    when('/newcar', {
-      templateUrl: 'partials/partial3',
-      controller: 'MyCtrl3'
-    }).
-    otherwise({
-      redirectTo: '/cars'
-    });
+    'myApp.controllers',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives'
+])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/cars', {
+                templateUrl: 'partials/partial1',
+                controller: 'MyCtrl1'
+            })
+            .when('/map', {
+                templateUrl: 'partials/partial2',
+                controller: 'MyCtrl2'
+            })
+            .when('/newcar', {
+                templateUrl: 'partials/partial3',
+                controller: 'MyCtrl3'
+            })
+            .otherwise({
+                redirectTo: '/cars'
+            });
 
-  $locationProvider.html5Mode(true);
-});
+        $locationProvider.html5Mode(true);
+    });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,47 +2,47 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
+angular.module('myApp.controllers', [])
+    .controller('AppCtrl', function ($scope, $http) {
 
-    $http({
-      method: 'GET',
-      url: '/api/name'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.name = data.name;
-    }).
-    error(function (data, status, headers, config) {
-      $scope.name = 'Error!'
+        $http({
+            method: 'GET',
+            url: '/api/name'
+        })
+            .success(function (data, status, headers, config) {
+                $scope.name = data.name;
+            })
+            .error(function (data, status, headers, config) {
+                $scope.name = 'Error!';
+            });
+
+    })
+    .controller('MyCtrl1', function ($scope, $http) {
+        $http({
+            method: 'GET',
+            url: '/api/cars'
+        })
+            .success(function (data, status, headers, config) {
+                $scope.cars = data.cars;
+            })
+            .error(function (data, status, headers, config) {
+                throw new Error('Cannot get cars!');
+            });
+        $scope.test = [ 1,2,3,4,5 ];
+
+    })
+    .controller('MyCtrl2', function ($scope) {
+        // write Ctrl here
+    })
+    .controller('MyCtrl3', function ($scope,$location) {
+        // write Ctrl here
+        $scope.message='';
+        $scope.addCar = function()
+        {
+            // push to server
+            $scope.message = 'Thanks, the car ' + $scope.car.name +
+                ' has been registered!';
+            // $location.url('/');
+        };
+
     });
-
-  }).
-  controller('MyCtrl1', function ($scope, $http) {
-    $http({
-      method: 'GET',
-      url: '/api/cars'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.cars = data.cars;
-    }).
-    error(function (data, status, headers, config) {
-        throw new Error('Cannot get cars!');
-    });
-    $scope.test = [ 1,2,3,4,5 ];
-
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
-  }).
-  controller('MyCtrl3', function ($scope,$location) {
-    // write Ctrl here
-    $scope.message='';
-    $scope.addCar = function()
-    {
-        // push to server
-        $scope.message = 'Thanks, the car ' + $scope.car.name +
-            ' has been registered!';
-        // $location.url('/');
-    };
-
-  });
