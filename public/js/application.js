5c07328f0e84484420499f73032567bfa33d835e
Jacques KAISER <jacko.kaiser@gmail.com>
2014-02-27 13:03:02 +1100

    added loader

--- public/js/application.js
+++ public/js/application.js
@@ -15,6 +15,5 @@
 //= require lib/angular/angular.min
 //= require lib/angular/angular-resource.min
 //= require lib/moment/min/moment.min.js
-//= require lib/nprogress.js
 
 //= require air-car-and-car
