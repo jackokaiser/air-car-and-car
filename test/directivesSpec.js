5614a8435bd0ae0a894cdaf68ca840cae4e4bc11
Jacques KAISER <jacko.kaiser@gmail.com>
2014-03-03 13:41:07 +1100

    more tests

--- test/directivesSpec.js
+++ test/directivesSpec.js
@@ -2,16 +2,18 @@
 
 /* jasmine specs for directives go here */
 
+
+
 xdescribe('directives', function() {
   beforeEach(module('myApp.directives'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
+  describe('daterange', function() {
+    it('should set date', function() {
       module(function($provide) {
-        $provide.value('version', 'TEST_VER');
+        // $provide.value('version', 'TEST_VER');
       });
       inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
+        var element = $compile('<span daterange></span>')($rootScope);
         expect(element.text()).toEqual('TEST_VER');
       });
     });
--- test/servicesSpec.js
+++ test/servicesSpec.js
@@ -2,13 +2,43 @@
 
 /* jasmine specs for services go here */
 
-xdescribe('service', function() {
-  beforeEach(module('myApp.services'));
+describe('service', function() {
+    beforeEach(module('myApp.services'));
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+    describe('ErrorService', function() {
+        it('should be able to set and clear message', inject(function(ErrorService) {
+            var msg = 'this is an error!';
+            ErrorService.setError(msg);
+            expect(ErrorService.errorMessage).toEqual(msg);
+            ErrorService.clear();
+            expect(ErrorService.errorMessage).toBeNull();
+        }));
+    });
+
+
+    describe('User', function() {
+        it('should get resource', inject(function(User,_$httpBackend_) {
+            var usrId = 5;
+            _$httpBackend_.expectGET('/api/account/'+usrId).respond(
+                new User(usrId)
+            );
+            User.get({id : usrId});
+            _$httpBackend_.flush();
+        }));
+    });
+
+    describe('Car', function() {
+        it('should get resource', inject(function(Car,_$httpBackend_) {
+            var carId = 5;
+            _$httpBackend_.expectGET('/api/cars/'+carId).respond(
+                new Car(carId)
+            );
+            Car.get({id : carId});
+            _$httpBackend_.flush();
+        }));
+    });
+
+
+
 });
